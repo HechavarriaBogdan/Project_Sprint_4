b273a3416c7a0ac21cef59adecf5da0f539affd9
bogdan diakov <bogdandiakov@gmail.com>
2024-08-18 06:32:00 +0100

    Удалены все данные из ветки main

--- README.md
+++
@@ -1,5 +0,0 @@
-## Запуск для Firefox
-
-```bash
-mvn clean test -Dbrowser=firefox
-```
--- src/main/java/praktikum/EnvConfig.java
+++
@@ -1,8 +0,0 @@
-package praktikum;
-
-public class EnvConfig {
-    public static final String BASE_URL = "https://qa-scooter.praktikum-services.ru/";
-    public static final int CHROME_IMPLICIT_WAIT = 5;
-    public static final int FIREFOX_IMPLICIT_WAIT = 10;
-    public static final int EXPLICIT_WAIT = 15;
-}
